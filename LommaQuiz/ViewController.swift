002fd17c3fc44fb8c7546835d7b5624be8cc8809
jörgen persson <jorgen_raby@icloud.com>
2021-12-06 18:06:21 +0100

    Initial Commit

---
+++ LommaQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LommaQuiz
+//
+//  Created by jörgen persson on 2021-12-06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
